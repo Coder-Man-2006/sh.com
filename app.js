9ca11d13ea6f490100dee58a9fd2b50183a2884e
Coder-Man-2006 <jatoujoseph@gmail.com>
2023-05-07 13:07:39 -0400

    added JS code for effects

--- app.js
+++ app.js
@@ -13,3 +13,27 @@ function moveLeft() {
     const pics = document.querySelector('.pic');
     pics.style.transform = 'translateX(-100.5%)';
   }
+
+let counter = 0
+const more = document.querySelector('.read-more')
+const about2 = document.querySelector('.about-body-2')
+const aboutdiv = document.querySelector('.about')
+
+function readCount () {
+  about2.style.display = 'block';
+  aboutdiv.style.height = '900px';
+  aboutdiv.style.transition = '9s ease-in-out'
+
+function readLess () {
+  about2.style.display = 'none';
+  aboutdiv.style.height = '625px';
+  aboutdiv.style.transition = '9s ease-in-out'
+}
+
+if (counter === 1) {
+  readMore()
+}
+else if (counter === 0) {
+  readLess()
+}
+}
