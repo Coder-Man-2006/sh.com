1e30133c167ef5899d441fbac14ba1f10bc90074
Coder-Man-2006 <jatoujoseph@gmail.com>
2023-05-08 11:12:23 -0400

    made changes to read more/less effect

--- app.js
+++ app.js
@@ -14,26 +14,30 @@ function moveLeft() {
     pics.style.transform = 'translateX(-100.5%)';
   }
 
-let counter = 0
+const counter = 0
 const more = document.querySelector('.read-more')
 const about2 = document.querySelector('.about-body-2')
 const aboutdiv = document.querySelector('.about')
+
 
 function readCount () {
   about2.style.display = 'block';
   aboutdiv.style.height = '900px';
   aboutdiv.style.transition = '9s ease-in-out'
+  counter += 1
+}  
 
 function readLess () {
   about2.style.display = 'none';
   aboutdiv.style.height = '625px';
   aboutdiv.style.transition = '9s ease-in-out'
+  counter += 1
 }
 
-if (counter === 1) {
+if (counter % 2 == 0)  {
   readMore()
 }
-else if (counter === 0) {
+else if (counter % 2 != 0) {
   readLess()
 }
-}
+
